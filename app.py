ecb27a81ca6374ef80f0af2d7fbf65c6566290d9
jadam1 <jadam1@kpmg.com>
2017-04-27 16:48:48 +1000

    added power bi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    response_body = 'Hello World'
+    response_body = '<iframe width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiNjI1ZTI2YWItNjdhOC00ZWVhLWI4ZTctYzRjZTAyYmEzNmQ5IiwidCI6ImViOWExMWM5LTJiMTctNDMzZi1hOTJhLTA4MjQ0OGVlZGVlZiJ9" frameborder="0" allowfullscreen="true"></iframe>
     yield response_body.encode()
 
 if __name__ == '__main__':
